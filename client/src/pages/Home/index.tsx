06ea171c6fa5631b0f196e0078b7035044da1c49
tldrd0117 <tldrd0117@gmail.com>
2020-05-17 00:03:40 +0900

    am

---
+++ client/src/pages/Home/index.tsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react"
+import {
+    Alignment,
+    Button,
+    Classes,
+    H5,
+    Navbar,
+    NavbarDivider,
+    NavbarGroup,
+    NavbarHeading,
+    Switch,
+    InputGroup,
+} from "@blueprintjs/core";
+import classNames from 'classnames/bind';
+// import styles from "./home.scss"
+
+// const cx = classNames.bind(styles)
+
+export default class Home extends Component {
+    render(){
+        return (
+            <></>
+            // <div className={cx("content")}></div>
+        )
+    }
+}
