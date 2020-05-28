681ce7b0a26719f9812126a22ff79e32f1eea130
tldrd0117 <tldrd0117@gmail.com>
2020-05-28 23:25:31 +0900

    qq

--- server/src/services/AuthService.ts
+++ server/src/services/AuthService.ts
@@ -1,13 +1,16 @@
 import { Service } from "typedi";
 import User from "../models/user";
 import argon2 from 'argon2';
+import { WhereOptions } from "sequelize";
 
 @Service()
 export default class AuthService{
-    async signIn(email, password){
+    async signIn(email: string, password: string){
         try{
             const user = await User.findOne({
-                where: email
+                where: {
+                    email
+                }
             })
             if(user){
                 const correct = argon2.verify(user.password, password);
